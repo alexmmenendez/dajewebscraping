bf87e8c697bd6ff3ee0ed0f01ffc704056f5308d
alexmenendezdocket <alex.menendez@docket.com.br>
2019-05-06 15:48:03 -0300

    first commit

---
+++ src/main/java/br/com/dajescraping/dajescraping/DajescrapingApplication.java
@@ -0,0 +1,13 @@
+package br.com.dajescraping.dajescraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DajescrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DajescrapingApplication.class, args);
+	}
+
+}
